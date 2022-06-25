bebcd11157ddb8e610e34f52c2efc88128b4a8d2
Aleksandr Komarov <ak@akxcv.com>
2022-06-25 22:38:34 +0400

    music-driven colours

--- spotify.js
+++ spotify.js
@@ -1,25 +1,54 @@
 "use strict";
 
 (function () {
+  const body = document.querySelector('body')
   const root = document.querySelector('.spotify-now-playing')
+  const fac = new FastAverageColor()
 
   function renderNowPlaying() {
-    fetch('https://jerry.the418.gg/akxcvapi/spotify/now_playing')
+    return fetch('https://jerry.the418.gg/akxcvapi/spotify/now_playing')
       .then(function (res){ return res.json() })
       .then(function (data) {
-        if (data.is_playing) {
-          root.innerHTML =
-            '<div class="label">Now Playing</div>' +
-            '<a href="' + data.item.external_urls.spotify + '">' +
-            '<img src="' + data.item.album.images.find(function (i) { return i.height <= 64 }).url + '" />' +
-            '<div class="track-name">' + data.item.artists.map(function (artist) { return artist.name }).join(', ') + ' &mdash; ' + data.item.name + '</div></a>'
-        } else {
-          root.innerHTML = ''
-        }
+        return new Promise((resolve, reject) => {
+          if (data.is_playing) {
+            const img = document.createElement('img')
+            img.src = data.item.album.images.find(function (i) { return i.height <= 64 }).url
+            img.crossOrigin = "anonymous"
+            img.onload = function (_e) {
+              fac.getColorAsync(img, { algorithm: 'dominant' })
+                .then(function (color) {
+                  root.innerHTML =
+                    '<div class="label">Now Playing</div>' +
+                    '<a href="' + data.item.external_urls.spotify + '">' +
+                    img.outerHTML +
+                    '<div class="track-name">' + data.item.artists.map(function (artist) { return artist.name }).join(', ') + ' &mdash; ' + data.item.name + '</div></a>'
+
+                  body.style.backgroundColor = color.rgba
+                  if (color.isDark) {
+                    body.classList.add('dark')
+                  } else {
+                    body.classList.remove('dark')
+                  }
+
+                  root.style.backgroundColor = color.rgba
+
+                  resolve()
+                })
+            }
+            img.onerror = reject
+          } else {
+            root.innerHTML = ''
+            body.style.backgroundColor = null
+            body.classList.remove('dark')
+            resolve()
+          }
+        })
       })
   }
 
-  renderNowPlaying()
+  renderNowPlaying().then(function () {
+    body.classList.remove('hidden')
+  })
 
   setInterval(renderNowPlaying, 3000)
 })()
